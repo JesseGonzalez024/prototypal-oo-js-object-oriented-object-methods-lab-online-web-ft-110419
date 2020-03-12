fb44ab6263bb24175339a9fbd5fc2ae3c544d980
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-03-12 00:20:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function BoardMember(name, homestate, training){
     return "You will see great things from Scuber."
   }
   
-  function sayHi(){
-     return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+  BoardMember.prototype.sayHi = function() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
   }
 }
