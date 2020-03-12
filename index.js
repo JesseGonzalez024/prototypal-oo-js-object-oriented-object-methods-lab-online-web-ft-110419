9a53a410ab69993430efe05ff21ebafe08aefa83
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-03-12 00:17:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function BoardMember(name, homestate, training){
     return "I like to help people."
   }
   
-   BoardMember.prototype.doCharity = function() {
+   BoardMember.prototype.releasePressStatemen = function() {
     return "I like to help people."
   }
   
